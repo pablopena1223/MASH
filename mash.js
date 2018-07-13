9e543f9dabbd6b22624a75afe34e841ebfd76822
pablopena1223 <lito23.pjp@gmail.com>
2018-07-13 11:02:08 -0700

    Version 2

--- mash.js
+++ mash.js
@@ -1,6 +1,15 @@
 function mash(){
-    return "house";
+    return getHome();
+
 }
 
 
 console.log(mash());
+
+function getHome(){
+    const places = ["Mansion","Apartment","Shack","House"];
+    const randomValue = Math.floor(Math.random() * places.length);
+    return places[randomValue];
+}
+
+console.log(getHome());
